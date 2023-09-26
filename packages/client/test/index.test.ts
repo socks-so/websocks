93a5385b7e62ca92eb6f522c81c659e11f4a4762
ChiChuRita <ra.singh069@gmail.com>
2023-09-26 22:17:21 +0200

    add client packages

--- packages/client/test/index.test.ts
+++ packages/client/test/index.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import z from "zod";
 
-describe("server", () => {
+describe("client", () => {
   it("should work", () => {
     expect(true).toBe(true);
   });
