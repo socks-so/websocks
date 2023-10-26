516c7cf0a7fdc1a1c2c2b90a11faa57e1e045e7e
ChiChuRita <ra.singh069@gmail.com>
2023-10-27 00:08:46 +0200

    add socks adapter

--- packages/server/src/adapter/socks.ts
+++ packages/server/src/adapter/socks.ts
@@ -7,33 +7,47 @@ interface SocksAdapterConfig {
 }
 
 interface Event {
+  wid: string;
   type: string;
   payload: unknown;
 }
 
+import { handleMessage } from "../message-handler";
+
 export function createSocksAdapter(config: SocksAdapterConfig) {
   return {
-    to(wid: string, data: unknown) {},
+    to(wid: string, data: unknown) {
+      //api.socks.so/to -> { wid, message }
+    },
 
     toRoom(rid: string, data: unknown) {
-      // ...
+      //api.socks.so/to-room -> { rid, message }
     },
 
     broadcast(data: unknown) {
-      // ...// ...
+      //api.socks.so/broadcast { message }
     },
 
     join(wid: string, rid: string) {
-      // ...
+      //api.socks.so/join -> { wid, rid }
     },
 
     leave(wid: string, rid: string) {
-      // ...
+      //api.socks.so/leave -> { wid, rid }
     },
 
     create(messageMap) {
       const handler: Handler<Event> = async (event, context) => {
-        const { type, payload } = event;
+        const { wid, type, payload } = event;
+        const data = { type, payload };
+
+        const message = messageMap.get(type);
+
+        if (!message) {
+          throw new Error(`Unknown message type: ${type}`);
+        }
+
+        handleMessage(message, data, wid);
       };
 
       return { handler };
