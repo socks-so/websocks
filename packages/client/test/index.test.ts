4f05d98b12f26d38956671ac077cec5d31d0edda
Liudvikas Zekas <liudve612@gmail.com>
2023-11-17 18:04:33 +0100

    clean up client

--- packages/client/test/index.test.ts
+++ packages/client/test/index.test.ts
@@ -1,41 +1,10 @@
 import { describe, it, expect } from "vitest";
 
-import { client } from "../src/node/index";
-import { init } from "../../server/src/index";
-import { createNodeAdapter } from "../../server/src/adapters/node";
-import { z } from "zod";
-import { WebSocketServer } from "ws";
+import { client } from "../src/index";
+import { Schema } from "./server";
 
 describe("client", () => {
   it("should work", () => {
-    const wss = new WebSocketServer({ port: 8080 });
-
-    const s = init({
-      context: () => ({}),
-      adapter: createNodeAdapter(wss),
-    });
-
-    const sender = s.sender.messages({
-      test: s.sender.message().payload(z.string()),
-    });
-
-    const receiver = s.receiver.messages({
-      test: s.receiver
-        .message()
-        .payload(z.string())
-        .on(async ({ payload }) => {
-          console.log("Message received:", payload);
-          await sender.test("Server sending message! " + payload).broadcast();
-        }),
-    });
-
-    const server = s.create({
-      receiverMessages: receiver,
-      senderMessages: sender,
-    });
-
-    type Schema = typeof server.schema;
-
     const cli = client<Schema>("ws://localhost:8080");
 
     cli.on.test((payload) => {
